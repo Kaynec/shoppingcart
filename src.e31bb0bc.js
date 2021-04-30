3c8a6abb001bcd206f5a66a575f4e0c0328b202c
kaynec <once462@gmail.com>
2021-04-30 18:05:19 +0000

    changed the media query for phone

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -41311,7 +41311,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33507" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33115" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
