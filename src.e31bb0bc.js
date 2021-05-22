1996713fe1795ffab3f430b251ba0a7880a97c15
kaynec <once462@gmail.com>
2021-05-22 12:39:18 +0000

    changed a media query

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -41311,7 +41311,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33115" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44677" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
